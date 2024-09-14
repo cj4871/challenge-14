3d70ce3692e56ac918d3d8fcb39c445088869a63
cj4871 <146256795+cj4871@users.noreply.github.com>
2024-09-13 22:50:43 -0500

    added last

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,12 @@
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log('server started!')
-});
+require('dotenv').config();
+const Sequelize = require('sequelize');
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(process.env.BLOG_DB, process.env.BLOG_USER, process.env.BLOG_PW, {
+      host: 'localhost',
+      dialect: 'postgres',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+module.exports = sequelize;
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+sequelize.sync().then(() => {
+    app.listen(PORT, () =>{
+        console.log("server Started!");
+    })
+});
